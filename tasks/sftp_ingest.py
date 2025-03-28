215553a491ee97b89495b942858501cb07cbb580
twais <brian.otieno709@gmail.com>
2025-03-28 15:44:59 +0300

    nit: fix styling

--- tasks/sftp_ingest.py
+++ tasks/sftp_ingest.py
@@ -1,14 +1,17 @@
-
 import os
 import pysftp
+
 
 class SFTPIngest:
     """
     Handles the ingestion of data from a specified SFTP location.
     This class connects to an SFTP server, retrieves files, and processes them as needed.
     """
+
     def __init__(self, sftp_host, sftp_user, sftp_pass):
-        with pysftp.Connection(sftp_host, username=sftp_user, password=sftp_pass) as sftp:
+        with pysftp.Connection(
+            sftp_host, username=sftp_user, password=sftp_pass
+        ) as sftp:
             self.sftp = sftp
 
     def get_files(self, remote_dir, local_dir):
@@ -24,7 +27,7 @@ class SFTPIngest:
                 os.makedirs(local_dir)
             # Get only csv files
             file_list = self.sftp.listdir(remote_dir)
-            file_list = [f for f in file_list if f.endswith('.csv')]
+            file_list = [f for f in file_list if f.endswith(".csv")]
             # Retrieve files
             for file in file_list:
                 remote_file = os.path.join(remote_dir, file)
--- tests/test_sftp_ingest.py
+++ tests/test_sftp_ingest.py
@@ -2,10 +2,13 @@ import unittest
 from unittest.mock import patch, MagicMock
 from tasks.sftp_ingest import SFTPIngest
 
+
 class TestSFTPIngest(unittest.TestCase):
     def setUp(self):
         # Patch pysftp.Connection to prevent actual SFTP connections
-        self.pysftp_patcher = patch("tasks.sftp_ingest.pysftp.Connection", autospec=True)
+        self.pysftp_patcher = patch(
+            "tasks.sftp_ingest.pysftp.Connection", autospec=True
+        )
         self.mock_connection_class = self.pysftp_patcher.start()
         self.addCleanup(self.pysftp_patcher.stop)
 
@@ -28,16 +31,23 @@ class TestSFTPIngest(unittest.TestCase):
         # Mock the behavior of get_files
         self.mock_sftp_ingest.get_files.return_value = ["file1.csv", "file2.csv"]
         self.sftp_ingest.get_files("/remote_dir", "/local_dir")
-        self.mock_sftp_ingest.get_files.assert_called_once_with("/remote_dir", "/local_dir")
-        self.assertEqual(self.sftp_ingest.get_files("/remote_dir", "/local_dir"), ["file1.csv", "file2.csv"])
+        self.mock_sftp_ingest.get_files.assert_called_once_with(
+            "/remote_dir", "/local_dir"
+        )
+        self.assertEqual(
+            self.sftp_ingest.get_files("/remote_dir", "/local_dir"),
+            ["file1.csv", "file2.csv"],
+        )
 
     def test_get_files_file_not_found(self):
-         # Mock behavior of get_files when FileNotFoundError is raised
+        # Mock behavior of get_files when FileNotFoundError is raised
         self.mock_sftp_ingest.get_files.side_effect = FileNotFoundError
         with self.assertRaises(FileNotFoundError):
             self.sftp_ingest.get_files("/remote_dir", "/local_dir")
         # verify that mocked method was called with the correct arguments
-        self.mock_sftp_ingest.get_files.assert_called_once_with("/remote_dir", "/local_dir")
+        self.mock_sftp_ingest.get_files.assert_called_once_with(
+            "/remote_dir", "/local_dir"
+        )
 
     def test_get_files_exception(self):
         # Mock behavior of get_files when exception is raised
@@ -45,5 +55,6 @@ class TestSFTPIngest(unittest.TestCase):
         with self.assertRaises(Exception):
             self.sftp_ingest.get_files("/remote_dir", "/local_dir")
         # verify that mocked method was called with the correct arguments
-        self.mock_sftp_ingest.get_files.assert_called_once_with("/remote_dir", "/local_dir")
-
+        self.mock_sftp_ingest.get_files.assert_called_once_with(
+            "/remote_dir", "/local_dir"
+        )
