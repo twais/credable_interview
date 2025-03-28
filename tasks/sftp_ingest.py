256c3e81a112f541e3ec285c6c92e0e9261e817a
twais <brian.otieno709@gmail.com>
2025-03-28 17:35:59 +0300

    introduce pipeline script, fix tests

--- tasks/sftp_ingest.py
+++ tasks/sftp_ingest.py
@@ -9,12 +9,16 @@ class SFTPIngest:
     """
 
     def __init__(self, sftp_host, sftp_user, sftp_pass):
-        with pysftp.Connection(
-            sftp_host, username=sftp_user, password=sftp_pass
-        ) as sftp:
-            self.sftp = sftp
+        try:
+            # Establish SFTP connection
+            self.sftp = pysftp.Connection(
+                sftp_host, username=sftp_user, password=sftp_pass
+            )
+        except Exception as e:
+            print(f"Failed to connect to SFTP server: {e}")
+            self.sftp = None
 
-    def get_files(self, remote_dir, local_dir):
+    def get_files(self, remote_dir, local_dir, file_name):
         """
         Retrieves files from the SFTP server and saves them to the local directory. Retrieves only CSV files.
         :param remote_dir: The directory on the SFTP server to retrieve files from.
@@ -25,18 +29,19 @@ class SFTPIngest:
             # Check if the local directory exists, if not create it
             if not os.path.exists(local_dir):
                 os.makedirs(local_dir)
-            # Get only csv files
-            file_list = self.sftp.listdir(remote_dir)
-            file_list = [f for f in file_list if f.endswith(".csv")]
-            # Retrieve files
-            for file in file_list:
-                remote_file = os.path.join(remote_dir, file)
-                local_file = os.path.join(local_dir, file)
-                self.sftp.get(remote_file, local_file)
-            return file_list
+            # Get only csv file with specified name
+            self.sftp.get(f"{remote_dir}/{file_name}", f"{local_dir}/{file_name}")
+            return True
+
         except FileNotFoundError as e:
             # Handle file not found error
             print(f"Error: {e}")
         except Exception as ex:
             # Handle other exceptions
             print(f"Error: {ex}")
+
+    def close(self):
+        """Closes the SFTP connection."""
+        if self.sftp:
+            self.sftp.close()
+            self.sftp = None
